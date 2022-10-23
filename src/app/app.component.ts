e14179fdf9e8c746f29fc6c6205e3c4858ad8162
gdpnei2002 <joseneilson25@gmail.com>
2022-10-23 03:19:20 -0300

    Compartilhamento de dados - #07

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = "joaquim"
+
+  userData = {
+    email: "joaquim@email.com",
+    role: "admin"
+  }
   title = 'curso-angular';
+
 }
