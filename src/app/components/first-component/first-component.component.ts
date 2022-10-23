66bd6d6696fb5602f8fd2c90418825e1b024b0f3
gdpnei2002 <joseneilson25@gmail.com>
2022-10-23 02:59:06 -0300

    Dados no template ep #5

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponentComponent implements OnInit {
   name: string ="seu nome"
+  age: number = 30
+  job = "Programador"
+  hobbies = ["Correr", "jogar", "Estudar"]
 
   constructor() { }
 
